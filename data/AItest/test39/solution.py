e7844cf335988b058e0c1110edcd934a4e70ee2c
MichaelYang-lyx <yang.5915@buckeyemail.osu.edu>
2024-05-17 12:02:51 +0800

    almost done everything

--- data/AItest/test39/solution.py
+++ data/AItest/test39/solution.py
@@ -1,4 +1,3 @@
-
 def is_prime(num):
     if num < 2:
         return False
@@ -8,25 +7,18 @@ def is_prime(num):
     return True
 
 def prime_fib(n: int):
-    def fibonacci(num):
-        if num <= 1:
-            return num
-        else:
-            return fibonacci(num - 1) + fibonacci(num - 2)
+    def fibonacci(n):
+        fibs = [0, 1]
+        for i in range(2, n+1):
+            fibs.append(fibs[-1] + fibs[-2])
+        return fibs[n]
 
     count = 0
     num = 1
     while count < n:
-        num += 1
         fib_num = fibonacci(num)
         if is_prime(fib_num):
             count += 1
+        num += 1
 
     return fib_num
-
-# Test cases
-print(prime_fib(1)) # Output: 2
-print(prime_fib(2)) # Output: 3
-print(prime_fib(3)) # Output: 5
-print(prime_fib(4)) # Output: 13
-print(prime_fib(5)) # Output: 89
